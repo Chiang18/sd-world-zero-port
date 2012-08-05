35bef7c06e6a8358104a976113b26f9237a9dc03
qq627891580@gmail.com <qq627891580@gmail.com@4c7c0748-f989-d528-9519-dabe0ab66d1b>
2012-08-05 23:57:01 +0000

    @thirdparth文件夹gamebryo模块(上次提交失败，补充提交) 1.添加了6个enum文件生成的h文件到Foundation 2.删除掉了几个用于工具的vsprops文件 3.将Foundation,ThirdPartyCode的中间路径改为$(EMERGENT_PATH)\Obj

---
+++ thirdparty/gamebryo/Source/Foundation/efd/ecrLogIDs.h
@@ -0,0 +1,27 @@
+//
+// This is a generated file.  You should not alter this file manually.
+
+//Source enum: m:\FB\depot\Gamebryo\3.0BR\Media\Enums\ecrLogIDs.enum
+
+#pragma once
+#ifndef EE_ecrLogIDs_h_INCLUDED
+#define EE_ecrLogIDs_h_INCLUDED
+
+#include <efd/ClassInfo.h>
+#include <efd/efdLogIDs.h>
+
+namespace efd
+{
+
+    static const efd::UInt16 kGamebryoGeneral0        =    25u; // 0x0019
+    static const efd::UInt16 kGamebryoGeneral1        =    26u; // 0x001A
+    static const efd::UInt16 kGamebryoGeneral2        =    27u; // 0x001B
+    static const efd::UInt16 kGamebryoMemory0         =    28u; // 0x001C
+    static const efd::UInt16 kGamebryoMemory1         =    29u; // 0x001D
+    static const efd::UInt16 kGamebryoMemory2         =    30u; // 0x001E
+    static const efd::UInt16 kGamebryoMemoryTimeStamp =    31u; // 0x001F
+    static const efd::UInt16 kGamebryoReserved        =    32u; // 0x0020
+
+} // end namespace efd
+
+#endif // EE_ecrLogIDs_h_INCLUDED
---
+++ thirdparty/gamebryo/Source/Foundation/efd/efdClassIDs.h
@@ -0,0 +1,58 @@
+//
+// This is a generated file.  You should not alter this file manually.
+
+//Source enum: m:\FB\depot\Gamebryo\3.0BR\Media\Enums\efdClassIDs.enum
+
+#pragma once
+#ifndef EE_efdClassIDs_h_INCLUDED
+#define EE_efdClassIDs_h_INCLUDED
+
+#include <efd/ClassInfo.h>
+
+namespace efd
+{
+
+    static const ClassID kCLASSID_MsgClassIDAndCatLogMask   = 3758096384u; // 0xE0000000
+    static const ClassID kCLASSID_XMLMemLogHandler          = 3758096385u; // 0xE0000001
+    static const ClassID kCLASSID_FileDestination           = 3758096386u; // 0xE0000002
+    static const ClassID kCLASSID_NetDestination            = 3758096387u; // 0xE0000003
+    static const ClassID kCLASSID_PrintDestination          = 3758096388u; // 0xE0000004
+    static const ClassID kCLASSID_SystemLogger              = 3758096389u; // 0xE0000005
+    static const ClassID kCLASSID_ConfigSource              = 3758096390u; // 0xE0000006
+    static const ClassID kCLASSID_CommandLineSource         = 3758096391u; // 0xE0000007
+    static const ClassID kCLASSID_IniSource                 = 3758096392u; // 0xE0000008
+    static const ClassID kCLASSID_EnvVarSource              = 3758096393u; // 0xE0000009
+    static const ClassID kCLASSID_IConfigSource             = 3758096394u; // 0xE000000A
+    static const ClassID kCLASSID_IMessage                  = 3758096395u; // 0xE000000B
+    static const ClassID kCLASSID_IStreamable               = 3758096396u; // 0xE000000C
+    static const ClassID kCLASSID_INetListener              = 3758096397u; // 0xE000000D
+    static const ClassID kCLASSID_EnvelopeMessage           = 3758096398u; // 0xE000000E
+    static const ClassID kCLASSID_StreamMessage             = 3758096399u; // 0xE000000F
+    static const ClassID kCLASSID_ServiceManager            = 3758096400u; // 0xE0000010
+    static const ClassID kCLASSID_AssetLocatorRequest       = 3758096401u; // 0xE0000011
+    static const ClassID kCLASSID_AssetLocatorResponse      = 3758096402u; // 0xE0000012
+    static const ClassID kCLASSID_AssetLocationMap          = 3758096403u; // 0xE0000013
+    static const ClassID kCLASSID_NetMessage                = 3758096404u; // 0xE0000014
+    static const ClassID kCLASSID_NetListener               = 3758096405u; // 0xE0000015
+    static const ClassID kCLASSID_NetworkStatus             = 3758096406u; // 0xE0000016
+    static const ClassID kCLASSID_ISectionEntry             = 3758096407u; // 0xE0000017
+    static const ClassID kCLASSID_ConfigSection             = 3758096408u; // 0xE0000018
+    static const ClassID kCLASSID_AssetTagger               = 3758096409u; // 0xE0000019
+    static const ClassID kCLASSID_AutoTagger_Mime           = 3758096410u; // 0xE000001A
+    static const ClassID kCLASSID_AutoTagger_PlatSample     = 3758096411u; // 0xE000001B
+    static const ClassID kCLASSID_AutoTagger_LogicalId      = 3758096412u; // 0xE000001C
+    static const ClassID kCLASSID_AutoTagger_PrecacheSample = 3758096413u; // 0xE000001D
+    static const ClassID kCLASSID_AutoTagger_Directory      = 3758096414u; // 0xE000001E
+    static const ClassID kCLASSID_NameResolutionMessage     = 3758096415u; // 0xE000001F
+    static const ClassID kCLASSID_NameResolutionRequest     = 3758096416u; // 0xE0000020
+    static const ClassID kCLASSID_NameResolutionResponse    = 3758096417u; // 0xE0000021
+    static const ClassID kCLASSID_NameResolutionLocalDone   = 3758096418u; // 0xE0000022
+    static const ClassID kCLASSID_NameResolutionRemoteDone  = 3758096419u; // 0xE0000023
+    static const ClassID kCLASSID_ProxyMessageTarget        = 3758096420u; // 0xE0000024
+    static const ClassID kCLASSID_HighPrecisionClock        = 3758096421u; // 0xE0000025
+    static const ClassID kCLASSID_StepHighPrecisionClock    = 3758096422u; // 0xE0000026
+    static const ClassID kCLASSID_Logger                    = 3758096423u; // 0xE0000027
+
+} // end namespace efd
+
+#endif // EE_efdClassIDs_h_INCLUDED
